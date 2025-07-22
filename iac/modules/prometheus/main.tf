f53665486f788a4c8da81ec59d669903c1050d74
kevinfinalboss <kevinmg50@gmail.com>
2025-07-22 16:10:42 +0000

    feat: Deploy Prometheus using Helm chart

---
+++ iac/modules/prometheus/main.tf
@@ -0,0 +1,33 @@
+data "aws_ssm_parameter" "htpasswd" {
+  name = "/prometheus/basic_auth/htpasswd"
+  with_decryption = true
+}
+
+resource "kubernetes_secret" "basic_auth" {
+  metadata {
+    name      = "prometheus-basic-auth"
+    namespace = var.namespace
+  }
+
+  data = {
+    auth = base64encode(data.aws_ssm_parameter.htpasswd.value)
+  }
+
+  type = "Opaque"
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  version    = var.chart_version
+  namespace  = var.namespace
+
+  values = [
+    templatefile("${path.root}/../helm-values/prometheus/by-version/${var.chart_version}/values.yaml", {
+      storage_size = var.storage_size
+    })
+  ]
+
+  timeout = 600
+}
